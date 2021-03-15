2bd904b3d8fb5f89e51faa5a363e6cd0862a45bd
Jamallc <weberrocha15@gmail.com>
2021-03-15 09:37:17 -0300

    page login

--- public/login.php
+++ public/login.php
@@ -1,18 +1,26 @@
-<?php 
-  include "./includes/head.php";
+<?php
+include "./includes/head.php";
 ?>
 
-<body class="fundoLogin">
+<body>
+  <div id="fundo-externo">
+    <div class="cor"></div>
+    <div id="fundo">
+      <img src="./img/feira.jpg" />
+    </div>
+  </div>
   <div class="container">
-    <section class="selecao">
-      <form action="" class="forme row">
-        <label for="email" class="form-label col-sm-12 col-md-3">E-mail:</label>
-        <input type="email" name="email" class="form-control col-sm-12 col-md-9" id="email" placeholder="email@exemplo.com">
+    <section class="selecao row">
+      <form action="" class="forme row col-11 col-sm-10 col-md-7 col-lg-6">
+        <h2 class="col-12 text-light d-flex justify-content-center">Login</h2>
 
-        <label for="senha" class="form-label col-sm-12 col-md-3">Senha:</label>
-        <input type="password" name="senha" class="form-control col-sm-12 col-md-9" id="senha">
+        <label for="email" class="form-label col-sm-12 col-md-3 col-lg-2 text-light">E-mail:</label>
+        <input type="email" name="email" class="form-control col-sm-12 col-md-9 col-lg-10" id="email" placeholder="email@exemplo.com">
 
-        <input type="submit" class="btn btn-success col-10" value="Login">
+        <label for="senha" class="form-label col-sm-12 col-md-3 col-lg-2 text-light">Senha:</label>
+        <input type="password" name="senha" class="form-control col-sm-12 col-md-9 col-lg-10" id="senha">
+
+        <input type="submit" class="btn btn-success col-12" value="Login">
       </form>
     </section>
   </div>
